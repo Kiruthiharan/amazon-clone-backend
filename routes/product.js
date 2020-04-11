a7c9a3d12a2f9453f985aa427b560707c44fa560
kiruthiharan <kiruthiharan@gmail.com>
2020-04-11 19:34:35 +0530

    category and owner retrieval along with product

--- routes/product.js
+++ routes/product.js
@@ -34,7 +34,7 @@ router.post('/products', upload.single("photo"), async (req, res) => {
 //GET - get all products
 router.get('/products', async(req,res) => {
     try {
-        let products = await Product.find();
+        let products = await Product.find().populate('owner category').exec();
 
         res.json({
             success: true,
